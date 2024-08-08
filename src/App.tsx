c277fd0ef8973597fa06a3c7fda60702004638b5
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-08-08 15:16:19 +0300

    fix3

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
 import { Route, Routes, useNavigate} from 'react-router-dom'
 import './App.css'
-import { AuthPage, LeadsPage, LoginPage, MainPage, ProfilePage} from './pages'
+import { LeadsPage, MainPage, ProfilePage} from './pages'
 import { observer } from 'mobx-react-lite'
-import userStore from './store/userStore'
-import { AuthLayout, Layout } from './shared/components'
-import { useEffect } from 'react'
+import { Layout } from './shared/components'
 import GeneratorPage from './pages/GeneratorPage/GeneratorPage'
 
 
 const App = observer(() => {
-  const nav = useNavigate()
-  useEffect(() => {
-    if (localStorage.getItem("currPage")) nav(`${localStorage.getItem("currPage")}`)
-  }, [])
+  const {} = useNavigate()
 
-  if (userStore.user.login === ""){
-    return(
-      <>
-      <AuthLayout>
-        <Routes>
-          <Route path='/auth' element={<AuthPage/>}  />
-          <Route path='/login' element={<LoginPage/>}  />
-        </Routes>
-      </AuthLayout>
-      </>
-    )
-  }
+  // if (userStore.user.login === ""){
+  //   return(
+  //     <>
+  //     <AuthLayout>
+  //       <Routes>
+  //         <Route path='/auth' element={<AuthPage/>}  />
+  //         <Route path='/login' element={<LoginPage/>}  />
+  //       </Routes>
+  //     </AuthLayout>
+  //     </>
+  //   )
+  // }
   
   return (
     <>
